4fdec3a932ae99ca3c5a110c9f4ee9dc5a27f894
sachTyagi <sachintyagi@novopay.in>
2017-07-25 16:36:36 +0530

    reducer for products created

---
+++ book-store/src/reducers/product.js
@@ -0,0 +1,83 @@
+import * as ActionTypes from '../constants/ActionTypes';
+
+var stock = [
+    {
+        id: '1',
+        name: 'book1',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '2',
+        name: 'book2',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '3',
+        name: 'book3',
+        description: 'description',
+        quantity: 10
+    },
+    {
+        id: '4',
+        name: 'book4',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '5',
+        name: 'book5',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '6',
+        name: 'book6',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '7',
+        name: 'book7',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '8',
+        name: 'book8',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '9',
+        name: 'book9',
+        description: 'description',
+        quantity: 5
+    },
+    {
+        id: '10',
+        name: 'book10',
+        description: 'description',
+        quantity: 5
+    }
+];
+
+var products = {
+    books: stock,
+    searchStr: ''
+};
+
+export default function productList(state = products, action){
+    switch (action.type){
+        case ActionTypes.SEARCH_PRODUCT:
+            state = {
+                books:  stock.filter((item) => (item.name.includes(action.value))),
+                searchStr: action.value
+            }
+            return state;
+        default :
+            return state;
+    }
+}
+
